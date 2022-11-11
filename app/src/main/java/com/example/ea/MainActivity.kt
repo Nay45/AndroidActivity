b79d5b2451d630d0a8804dc45d77de6eab70b4d6
Nay45 <nayottama45@gmail.com>
2022-11-11 08:12:17 +0700

    Detail Penjelasan

--- app/src/main/java/com/example/ea/MainActivity.kt
+++ app/src/main/java/com/example/ea/MainActivity.kt
@@ -10,14 +10,21 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    //private artinya variabel-variabel yang didefinisikan bisaa diakses dalam satu class.
+    //lateinit digunakan untuk menginisialisasi objek atau properti-properti yang digunakan.
+    //var artinya Object Mutable yang dapat di ubah isinya atau di ganti nilainya.
     private lateinit var edtWidth: EditText
     private lateinit var edtHeight: EditText
     private lateinit var edtLength: EditText
     private lateinit var btnCalculate: Button
     private lateinit var tvResult: TextView
+    //onCreate() adalah kondisi awal saat Activity baru diciptakan, biasanya dilakukan inisialisasi pada tahapan ini.
+    //savedInstanceState adalah metode untuk menerima parameter yang merupakan objek Bundle yang berisi status aktivitas yang sebelumnya disimpan.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //findViewById(R.id....) digunakan untuk mengenali id TextView yang sudah kita buat.
 
         edtWidth = findViewById(R.id.edt_width)
         edtHeight = findViewById(R.id.edt_height)
@@ -25,7 +32,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnCalculate = findViewById(R.id.btn_calculate)
         tvResult = findViewById(R.id.tv_result)
 
+        //setOnClickListener menjalankan suatu instruksi yang sudah ditentukan sebelumnya
+
         btnCalculate.setOnClickListener(this)
+
+        //pernyataan yang berfungsi jika savedInstanceState tidak kosong maka hasil akan ditampilkan.
 
         if (savedInstanceState != null) {
             val result = savedInstanceState.getString(STATE_RESULT) as String
@@ -33,14 +44,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    //onClick() berfungsi untuk menghandle saat tombol diklik.
     override fun onClick(v: View) {
         if  (v.id == R.id.btn_calculate) {
+            //val artinya Object Immutable yang tidak dapat di ubah isinya atau di ganti nilainya.
             val inputLength = edtLength.text.toString().trim()
             val inputWidth = edtWidth.text.toString().trim()
             val inputHeight = edtHeight.text.toString().trim()
 
+            //isEmptyFields adalah metode yang digunakan untuk mengecek isi kolom EditText.
             var isEmptyFields = false
 
+            //jika input kosong maka akan muncul pernyataan error "Field Ini Tidak Boleh Kosong".
             if (inputLength.isEmpty()) {
                 isEmptyFields = true
                 edtLength.error = "Field Ini Tidak Boleh Kosong"
@@ -56,6 +71,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 edtHeight.error = "Field Ini Tidak Boleh Kosong"
             }
 
+            //jika input tidak kosong maka hasil akan ditampilkan.
             if (!isEmptyFields) {
                 val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
                 tvResult.text = volume.toString()
@@ -63,10 +79,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    //companion object berfungsi untuk memberi variabel di suatu kelas agar bisa dipanggil tanpa melalui sebuah objek.
     companion object {
         private const val STATE_RESULT = "state_result"
     }
 
+    //onSaveInstanceState digunakan untuk situasi dimana Activity dapat dibuat ulang dengan menyimpan dan mengembalikan state Actibvity sebelumnya, misalnya saat screen rotation (perubahan orientasi layar).
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(STATE_RESULT, tvResult.text.toString())
